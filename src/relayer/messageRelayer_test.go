1fd1fb5af8410bd54f26a94e45c93db83e4d9014
Michael Street <mi.street@gmail.com>
2021-11-06 17:42:29 +0200

    wip tests

--- src/relayer/messageRelayer_test.go
+++ src/relayer/messageRelayer_test.go
@@ -90,182 +90,241 @@ func Test_single_subscriber(t *testing.T) {
 
 }
 
-func Test_all_subscribers_receives_all_messages_of_correct_type(t *testing.T) {
-	/* setup */
-	var wg sync.WaitGroup
-	// sub to start new round messages, bufferred because
-	// busy channels get their messages dropped
-	firstSubCh := make(chan domain.Message, 2)
-	gotStartNewRound := 0
-
-	secondSubCh := make(chan domain.Message, 2)
-	gotReceivedAnswer := 0
-
-	// use stub socket
-	responses := []network.NetworkResponse{
-		StartNewRoundResponse,
-		ReceivedAnswerResponse,
-		StartNewRoundResponse,
-		ReceivedAnswerResponse,
+func Test_multiple_subscribers(t *testing.T) {
+	makeTestCaseName := func(n string) string {
+		return fmt.Sprintf("%sall subscribers receive all messages of correct type", n)
 	}
-	socket := network.NewNetworkSocketStub(responses)
 
-	mr := NewDefaultMessageRelayer(socket)
+	testCases := []MessageRelayerServerTestCase{
+		{
+			Name:  "DefaultMessageRelayer:",
+			Maker: NewDefaultMessageRelayer,
+		},
+	}
 
-	// subscribe each channel to the relayer
-	mr.SubscribeToMessage(domain.StartNewRound, firstSubCh)
-	mr.SubscribeToMessage(domain.ReceivedAnswer, secondSubCh)
+	makeTestCase := func(m MakeMessageRelayerServer) func(t *testing.T) {
+		return func(t *testing.T) {
+			/* setup */
+			var wg sync.WaitGroup
+			// sub to start new round messages, bufferred because
+			// busy channels get their messages dropped
+			firstSubCh := make(chan domain.Message, 2)
+			gotStartNewRound := 0
 
-	go mr.ListenAndRelay()
+			secondSubCh := make(chan domain.Message, 2)
+			gotReceivedAnswer := 0
 
-	/* actions */
-	// read each message from the channel and increment the count
-	wg.Add(1)
-	go func() {
-		for range firstSubCh {
-			gotStartNewRound++
+			// use stub socket
+			responses := []network.NetworkResponse{
+				StartNewRoundResponse,
+				ReceivedAnswerResponse,
+				StartNewRoundResponse,
+				ReceivedAnswerResponse,
+			}
+			socket := network.NewNetworkSocketStub(responses)
+
+			mr := NewDefaultMessageRelayer(socket)
+
+			// subscribe each channel to the relayer
+			mr.SubscribeToMessage(domain.StartNewRound, firstSubCh)
+			mr.SubscribeToMessage(domain.ReceivedAnswer, secondSubCh)
+
+			go mr.ListenAndRelay()
+
+			/* actions */
+			// read each message from the channel and increment the count
+			wg.Add(1)
+			go func() {
+				for range firstSubCh {
+					gotStartNewRound++
+				}
+				wg.Done()
+			}()
+
+			wg.Add(1)
+			go func() {
+				for range secondSubCh {
+					gotReceivedAnswer++
+				}
+				wg.Done()
+			}()
+
+			// assertions
+			// count of received StartNewRound and ReceivedAnswer messages should be two
+			wg.Wait()
+			require.Equal(t, 2, gotStartNewRound)
+			require.Equal(t, 2, gotReceivedAnswer)
 		}
-		wg.Done()
-	}()
+	}
 
-	wg.Add(1)
-	go func() {
-		for range secondSubCh {
-			gotReceivedAnswer++
-		}
-		wg.Done()
-	}()
-
-	// assertions
-	// count of received StartNewRound and ReceivedAnswer messages should be two
-	wg.Wait()
-	require.Equal(t, 2, gotStartNewRound)
-	require.Equal(t, 2, gotReceivedAnswer)
+	for _, tc := range testCases {
+		t.Run(makeTestCaseName(tc.Name), makeTestCase(tc.Maker))
+	}
 
 }
 
-func Test_relayer_continues_with_non_fatal_network_errors(t *testing.T) {
-	/* setup */
-	var wg sync.WaitGroup
-	// sub to start new round messages, bufferred because
-	// busy channels get their messages dropped
-	firstSubCh := make(chan domain.Message, 2)
-	gotStartNewRound := 0
-
-	secondSubCh := make(chan domain.Message, 2)
-	gotReceivedAnswer := 0
-
-	// use stub socket
-	responses := []network.NetworkResponse{
-		StartNewRoundResponse,
-		ReceivedAnswerResponse,
-		NetworkErrorResponse,
-		NetworkErrorResponse,
-		StartNewRoundResponse,
-		ReceivedAnswerResponse,
-		NetworkErrorResponse,
+func Test_multiple_subscribers_and_errors(t *testing.T) {
+	makeTestCaseName := func(n string) string {
+		return fmt.Sprintf("%srelayer continues with non fatal network errors", n)
 	}
-	socket := network.NewNetworkSocketStub(responses)
 
-	mr := NewDefaultMessageRelayer(socket)
+	testCases := []MessageRelayerServerTestCase{
+		{
+			Name:  "DefaultMessageRelayer:",
+			Maker: NewDefaultMessageRelayer,
+		},
+	}
 
-	// subscribe each channel to the relayer
-	mr.SubscribeToMessage(domain.StartNewRound, firstSubCh)
-	mr.SubscribeToMessage(domain.ReceivedAnswer, secondSubCh)
+	makeTestCase := func(m MakeMessageRelayerServer) func(t *testing.T) {
+		return func(t *testing.T) {
+			/* setup */
+			var wg sync.WaitGroup
+			// sub to start new round messages, bufferred because
+			// busy channels get their messages dropped
+			firstSubCh := make(chan domain.Message, 2)
+			gotStartNewRound := 0
 
-	go mr.ListenAndRelay()
+			secondSubCh := make(chan domain.Message, 2)
+			gotReceivedAnswer := 0
 
-	/* actions */
-	// read each message from the channel and increment the count
-	wg.Add(1)
-	go func() {
-		for range firstSubCh {
-			gotStartNewRound++
+			// use stub socket
+			responses := []network.NetworkResponse{
+				StartNewRoundResponse,
+				ReceivedAnswerResponse,
+				NetworkErrorResponse,
+				NetworkErrorResponse,
+				StartNewRoundResponse,
+				ReceivedAnswerResponse,
+				NetworkErrorResponse,
+			}
+			socket := network.NewNetworkSocketStub(responses)
+
+			mr := NewDefaultMessageRelayer(socket)
+
+			// subscribe each channel to the relayer
+			mr.SubscribeToMessage(domain.StartNewRound, firstSubCh)
+			mr.SubscribeToMessage(domain.ReceivedAnswer, secondSubCh)
+
+			go mr.ListenAndRelay()
+
+			/* actions */
+			// read each message from the channel and increment the count
+			wg.Add(1)
+			go func() {
+				for range firstSubCh {
+					gotStartNewRound++
+				}
+				wg.Done()
+			}()
+
+			wg.Add(1)
+			go func() {
+				for range secondSubCh {
+					gotReceivedAnswer++
+				}
+				wg.Done()
+			}()
+
+			// assertions
+			// count of received StartNewRound and ReceivedAnswer messages should be two
+			wg.Wait()
+			require.Equal(t, 2, gotStartNewRound)
+			require.Equal(t, 2, gotReceivedAnswer)
 		}
-		wg.Done()
-	}()
+	}
 
-	wg.Add(1)
-	go func() {
-		for range secondSubCh {
-			gotReceivedAnswer++
-		}
-		wg.Done()
-	}()
-
-	// assertions
-	// count of received StartNewRound and ReceivedAnswer messages should be two
-	wg.Wait()
-	require.Equal(t, 2, gotStartNewRound)
-	require.Equal(t, 2, gotReceivedAnswer)
+	for _, tc := range testCases {
+		t.Run(makeTestCaseName(tc.Name), makeTestCase(tc.Maker))
+	}
 
 }
 
-func Test_multiple_subs_of_same_type_receive_all_messages_of_correct_type(t *testing.T) {
-	/* setup */
-	var wg sync.WaitGroup
-	// sub to start new round messages, bufferred because
-	// busy channels get their messages dropped
-	firstSubCh := make(chan domain.Message, 2)
-	gotStartNewRound := 0
-
-	secondSubCh := make(chan domain.Message, 2)
-	gotReceivedAnswer := 0
-
-	thirdSubCh := make(chan domain.Message, 2)
-	gotStartNewRoundToo := 0
-
-	// use stub socket
-	responses := []network.NetworkResponse{
-		StartNewRoundResponse,
-		ReceivedAnswerResponse,
-		NetworkErrorResponse,
-		NetworkErrorResponse,
-		StartNewRoundResponse,
-		ReceivedAnswerResponse,
-		NetworkErrorResponse,
+func Test_multiple_subscribers_same_topic(t *testing.T) {
+	makeTestCaseName := func(n string) string {
+		return fmt.Sprintf("%smultiple subscribers of same topic receive all messages of correct type", n)
 	}
-	socket := network.NewNetworkSocketStub(responses)
 
-	mr := NewDefaultMessageRelayer(socket)
+	testCases := []MessageRelayerServerTestCase{
+		{
+			Name:  "DefaultMessageRelayer:",
+			Maker: NewDefaultMessageRelayer,
+		},
+	}
 
-	// subscribe each channel to the relayer
-	mr.SubscribeToMessage(domain.StartNewRound, firstSubCh)
-	mr.SubscribeToMessage(domain.ReceivedAnswer, secondSubCh)
-	mr.SubscribeToMessage(domain.StartNewRound, thirdSubCh)
+	makeTestCase := func(m MakeMessageRelayerServer) func(t *testing.T) {
+		return func(t *testing.T) {
+			/* setup */
+			var wg sync.WaitGroup
+			// sub to start new round messages, bufferred because
+			// busy channels get their messages dropped
+			firstSubCh := make(chan domain.Message, 2)
+			gotStartNewRound := 0
 
-	go mr.ListenAndRelay()
+			secondSubCh := make(chan domain.Message, 2)
+			gotReceivedAnswer := 0
 
-	/* actions */
-	// read each message from the channel and increment the count
-	wg.Add(1)
-	go func() {
-		for range firstSubCh {
-			gotStartNewRound++
+			thirdSubCh := make(chan domain.Message, 2)
+			gotStartNewRoundToo := 0
+
+			// use stub socket
+			responses := []network.NetworkResponse{
+				StartNewRoundResponse,
+				ReceivedAnswerResponse,
+				NetworkErrorResponse,
+				NetworkErrorResponse,
+				StartNewRoundResponse,
+				ReceivedAnswerResponse,
+				NetworkErrorResponse,
+			}
+			socket := network.NewNetworkSocketStub(responses)
+
+			mr := NewDefaultMessageRelayer(socket)
+
+			// subscribe each channel to the relayer
+			mr.SubscribeToMessage(domain.StartNewRound, firstSubCh)
+			mr.SubscribeToMessage(domain.ReceivedAnswer, secondSubCh)
+			mr.SubscribeToMessage(domain.StartNewRound, thirdSubCh)
+
+			go mr.ListenAndRelay()
+
+			/* actions */
+			// read each message from the channel and increment the count
+			wg.Add(1)
+			go func() {
+				for range firstSubCh {
+					gotStartNewRound++
+				}
+				wg.Done()
+			}()
+
+			wg.Add(1)
+			go func() {
+				for range secondSubCh {
+					gotReceivedAnswer++
+				}
+				wg.Done()
+			}()
+
+			wg.Add(1)
+			go func() {
+				for range thirdSubCh {
+					gotStartNewRoundToo++
+				}
+				wg.Done()
+			}()
+
+			// assertions
+			// count of received StartNewRound and ReceivedAnswer messages should be two
+			wg.Wait()
+			require.Equal(t, 2, gotStartNewRound)
+			require.Equal(t, 2, gotStartNewRoundToo)
+			require.Equal(t, 2, gotReceivedAnswer)
+
 		}
-		wg.Done()
-	}()
+	}
 
-	wg.Add(1)
-	go func() {
-		for range secondSubCh {
-			gotReceivedAnswer++
-		}
-		wg.Done()
-	}()
+	for _, tc := range testCases {
+		t.Run(makeTestCaseName(tc.Name), makeTestCase(tc.Maker))
+	}
 
-	wg.Add(1)
-	go func() {
-		for range thirdSubCh {
-			gotStartNewRoundToo++
-		}
-		wg.Done()
-	}()
-
-	// assertions
-	// count of received StartNewRound and ReceivedAnswer messages should be two
-	wg.Wait()
-	require.Equal(t, 2, gotStartNewRound)
-	require.Equal(t, 2, gotStartNewRoundToo)
-	require.Equal(t, 2, gotReceivedAnswer)
 }
