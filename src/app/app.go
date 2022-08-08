458d5d518010c1fbff46fdce7f45fb3f9105bcda
Michael Street <5597260+MStreet3@users.noreply.github.com>
2022-08-07 20:21:20 -0400

    use a take consumer

--- src/app/app.go
+++ src/app/app.go
@@ -1,12 +1,9 @@
 package app
 
 import (
-	"errors"
-	"fmt"
-	"sync"
+	"context"
 
 	"github.com/mstreet3/message-relayer/domain"
-	"github.com/mstreet3/message-relayer/errs"
 	"github.com/mstreet3/message-relayer/network"
 	"github.com/mstreet3/message-relayer/relayer"
 	"github.com/mstreet3/message-relayer/utils"
@@ -18,47 +15,24 @@ message relayer.
 */
 func Start() {
 	var (
-		wg        sync.WaitGroup
-		responses = []network.NetworkResponse{
+		ctx, cancel = context.WithCancel(context.Background())
+		responses   = []network.NetworkResponse{
 			{
 				Message: &domain.Message{
 					Type: domain.ReceivedAnswer,
 				},
 			},
 		}
-		errCount = 0
-		msgCount = 0
-		ns       = network.NewNetworkSocketStub(responses)
-		mr       = relayer.NewDefaultMessageRelayer(ns)
+		ns = network.NewNetworkSocketStub(responses)
+		mr = relayer.NewDefaultMessageRelayer(ns)
 	)
 
 	go mr.Start()
 	defer mr.Stop()
+	defer cancel()
 
-	errCh := mr.Errors()
 	l := mr.Subscribe(domain.ReceivedAnswer)
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case err, open := <-errCh:
-				if !open {
-					return
-				}
-				if errors.Is(err, errs.FatalSocketError{}) {
-					errCount++
-				}
-			case <-l:
-				if msgCount == 5 {
-					return
-				}
-				msgCount++
-			}
-		}
-	}()
-	wg.Wait()
-
-	utils.DPrintf(fmt.Sprintf("Received %d fatal errors and %d messages\n", errCount, msgCount))
+	done := utils.Take(ctx.Done(), l, 5)
+	<-done
 }
--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,12 +1,29 @@
 package utils
 
-import "log"
+import (
+	"log"
+)
 
 const Debug = true
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
+func DPrintf(format string, a ...interface{}) {
 	if Debug {
 		log.Printf(format, a...)
 	}
-	return
+}
+
+// Take reads n values from a channel or is stopped
+func Take[T interface{}](stop <-chan struct{}, ch <-chan T, n int) <-chan struct{} {
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		for count := 0; count < n; count++ {
+			select {
+			case <-stop:
+				return
+			case <-ch:
+			}
+		}
+	}()
+	return done
 }
